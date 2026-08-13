7ffebd5c16c9ec4f8cc891933c2226834dc15c1c
Facugarciar0805 <facugarciar@gmail.com>
2026-08-13 11:43:10 -0300

    terminado navbar
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./router";
+import Navbar from "./components/NavBar.tsx";
 
 
 function App() {
   return (
     <>
         <BrowserRouter>
+            <Navbar/>
             <AppRouter/>
         </BrowserRouter>
     </>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+export default function Navbar () {
+    return <div className="bg-gray-200 w-full h-12 flex flex-col items-end">
+        <Link to="/">
+        <div className="w-1/12 h-full grid-cols-2 grid items-center justify-items-center pl-3">
+            <h2>Pokedex</h2>
+            <img className="w-full h-full object-contain pl-2 pt-2" src="/src/assets/pokedex.png" alt="logo" />
+        </div>
+        </Link>
+    </div>
+}
