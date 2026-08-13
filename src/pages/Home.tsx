bd978919267fdaa72f7a6a1a30a757d755fa0271
Facugarciar0805 <facugarciar@gmail.com>
2026-08-13 11:51:52 -0300

    agrego card para el pokemon detail
    Signed-off-by: Facugarciar0805 <facugarciar@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,15 @@ import PokemonResultsList from "../components/PokemonResultsList.tsx";
 
 function Home(){
     const {pokemons, loadingPokemons, errorLoading} = usePokemons();
-    return <div>
-        <SearchBar isLoading={false} onSearch={()=>{console.log("hola")}}/>
-        {loadingPokemons ? (
-            <p>Cargando...</p>
-        ) : errorLoading ? (
-            <p>{errorLoading}</p>
-        ) : (
-            <PokemonResultsList pokemons={pokemons} />
-        )}
-    </div>;
+    return <div className="flex flex-col items-center justify-center min-h-screen">
+            <SearchBar isLoading={false} onSearch={()=>{console.log("hola")}}/>
+            {loadingPokemons ? (
+                <p>Cargando...</p>
+            ) : errorLoading ? (
+                <p>{errorLoading}</p>
+            ) : (
+                <PokemonResultsList pokemons={pokemons} />
+            )}
+        </div>
 }
 export default Home;
